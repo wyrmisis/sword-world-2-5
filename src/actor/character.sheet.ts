ce747a462b6522c75651927abcc27262455384f3
wyrmisis <tim.snyder.work@gmail.com>
2023-09-21 00:19:08 -0400

    chore(character): Missed some files in the previous commit

--- src/actor/character.sheet.ts
+++ src/actor/character.sheet.ts
@@ -2,6 +2,10 @@
  * @file The backing class for the `character` Actor type's sheet
  */
 class CharacterSheet extends ActorSheet {
+  static get customFields () {
+    return ["sw-input, sw-meter"].join(", "); 
+  }
+
   get template() {
     // @ts-expect-error - game.system exists in v10+
     return `/systems/${game.system.id}/dist/templates/character.sheet.hbs`;
@@ -15,6 +19,14 @@ class CharacterSheet extends ActorSheet {
       width: 584,
       height: 800,
       tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".tab-group__container", initial: "summary" }]
+    });
+  }
+
+  activateListeners(html: JQuery<HTMLElement>): void {
+    super.activateListeners(html);
+
+    html.on("change", CharacterSheet.customFields, (e) => {
+      this._onChangeInput(e)
     });
   }
 }
